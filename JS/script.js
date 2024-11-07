978783f258686a73c454a0aef1bb8f2c258c55bc
raylann_lopes <raylanlopes@icloud.com>
2024-11-06 21:12:27 -0300

    git commit -m "Add scrollreveal.js and responsive design for mobile devices"

--- JS/script.js
+++ JS/script.js
@@ -2,7 +2,7 @@ var swiper = new Swiper(".mySwiper", {
   effect: "coverflow",
   grabCursor: true,
   centeredSlides: true,
-  loop:true,
+  loop: true,
   slidesPerView: "auto",
   coverflowEffect: {
     rotate: 0,
@@ -11,10 +11,41 @@ var swiper = new Swiper(".mySwiper", {
     modifier: 2.5,
     slideShadows: true,
   },
-  autoplay:{
-
-    delay:3000,
-    disableOnInteraction:false,
-  }
-
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
 });
+
+/*Animações*/
+
+const sr = ScrollReveal({
+  distance: "65px",
+  duration: 2600,
+  delay: 450,
+  reset: true,
+});
+
+sr.reveal(".image, .residence-text, .icons", { delay: 100, origin: "top" });
+sr.reveal(".contact", { delay: 450, origin: "right" });
+sr.reveal(".structure-video", { delay: 150, origin: "right" });
+sr.reveal(".image1", { delay: 300, origin: "right" });
+sr.reveal(".scroll-down", { delay: 500, origin: "right" });
+
+/*icones*/
+
+sr.reveal(".icon-1", { delay: 50, origin: "left" });
+sr.reveal(".icon-2", { delay: 100, origin: "top" });
+sr.reveal(".icon-3", { delay: 50, origin: "right" });
+sr.reveal(".icon-4", { delay: 50, origin: "bottom" });
+sr.reveal(".icon-5", { delay: 50, origin: "left" });
+sr.reveal(".icon-6", { delay: 50, origin: "top" });
+sr.reveal(".icon-7", { delay: 50, origin: "right" });
+sr.reveal(".icon-8", { delay: 50, origin: "bottom" });
+sr.reveal(".icon-9", { delay: 50, origin: "left" });
+
+/*Animação de reels*/
+
+sr.reveal(".video-1", { delay: 300, origin: "right" });
+sr.reveal(".video-2", { delay: 1000, origin: "right" });
+sr.reveal(".video-3", { delay: 1700, origin: "right" });
