a406a7f7eedf0f8dca87abbc855368ef24c09ecd
raylann_lopes <raylanlopes@icloud.com>
2024-11-26 09:08:37 -0300

    test

---
+++ php/usuario.php
@@ -0,0 +1,20 @@
+<?php
+
+require("connection.php");
+
+if (isset($_POST)) {
+
+    $nome = $_POST['nome'];
+    $cpf = $_POST['cpf'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $finalidade = $_POST['finalidade'];
+
+
+    $query = "INSERT INTO tblpessoa (nome, cpf, email, telefone, finalidade) VALUES ('$nome', '$sobrenome', '$email', '$telefone', '$finalidade')";
+
+    $stmt = $pdo->prepare($query);
+    $stmt->execute();
+
+    header("Location: index.html?criado=sucesso");
+}
