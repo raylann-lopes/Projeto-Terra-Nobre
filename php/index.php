43f53fd9c6503e4002cd193662ef7e274b2f0da5
raylann_lopes <raylanlopes@icloud.com>
2024-11-26 07:56:58 -0300

    feat: switch database

--- php/index.php
+++ php/index.php
@@ -7,4 +7,4 @@ $dbname		= "nserrana";
 
 /$conn = new PDO($servidor, $usuario, $senha, $dbname,array(PDO::ATTR_PERSISTENT => true)) or die("Connection Off: " . $conn->connect_error);
 */
-$conn = pg_connect("host=localhost port=5432 dbname=nova-serrana user=postgres password=2204");
+$conn = new PDO('mysql:host=186.195.143.1;port=3327;dbname=cadastro-nova-serrana', 'root', 'usbw', array(PDO::ATTR_PERSISTENT => true)) or die("Connection Off: " . $conn->connect_error);
